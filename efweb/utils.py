ce2e309bed9e4b90e029608609dfb5fc45548ed8
gauss <853934146@qq.com>
2018-04-21 10:56:23 +0800

    fix a bug

--- efweb/utils.py
+++ efweb/utils.py
@@ -4,4 +4,4 @@ import pkgutil
 
 data = pkgutil.get_data('efweb', 'data/error_status.dat')
 
-responses = {line.split(" ")[0]: " ".join(line.split(" ")[1:]) for line in data.decode("utf-8").split('\r\n')}
+responses = {int(line.split(" ")[0]): " ".join(line.split(" ")[1:]) for line in data.decode("utf-8").split('\r\n') if '×' not in line}
